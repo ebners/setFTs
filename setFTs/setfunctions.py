7241ea5029e7acfc9910cc203b3ec0efb0e6e310
ebners <ebners@ethz.ch>
2022-08-27 22:25:41 +0200

    chenges to docs

--- setFTs/setfunctions.py
+++ setFTs/setfunctions.py
@@ -56,7 +56,7 @@ class SetFunction():
     
 class WrapSignal(SetFunction):
     """Wrapper class for instantiating set functions with a full list of set function evaluations"""
-    def __init__(self,signal : List[float]):
+    def __init__(self,signal):
         """initializes setfunction with list
 
         :param signal: complete list of float outputs of a setfunction in lexicographical ordering
@@ -64,7 +64,7 @@ class WrapSignal(SetFunction):
         """
         pass
 
-    def __call__(self,indicator : npt.NDArray, count_flag : bool=True) -> npt.NDArray[np.float64]:
+    def __call__(self,indicator, count_flag =True) :
         """calls set function with np.array of indicator vectors
 
             :param indicators: two dimensional np.array with one indicator vector per row
@@ -137,7 +137,7 @@ class WrapSignal(SetFunction):
     
 class WrapSetFunction(SetFunction):
     """Wrapper class for instantiating set functions with a callable function"""
-    def __init__(self, s : Callable[[npt.NDArray],float],n, use_call_dict=False, use_loop=True):
+    def __init__(self, s,n, use_call_dict=False, use_loop=True):
         """init function
 
         :param s: callable function that takes a one-dimensional np.array and returns a float
@@ -150,7 +150,7 @@ class WrapSetFunction(SetFunction):
         """
         pass
         
-    def __call__(self, indicator : npt.NDArray, count_flag=True) -> npt.NDArray[np.float64]:
+    def __call__(self, indicator , count_flag=True):
         """calls set function with np.array of indicator vectors
 
             :param indicators: two dimensional np.array with one indicator vector per row
@@ -183,7 +183,7 @@ class WrapSetFunction(SetFunction):
 
 class SparseDSFTFunction(SetFunction):
 
-    def __init__(self, frequencies : npt.NDArray, coefficients : npt.NDArray[np.float64], model : str ,normalization_flag=False):
+    def __init__(self, frequencies , coefficients , model : str ,normalization_flag=False):
         """ Initializes a Sparse Fourier transformed set function object for the desired model
 
             :param frequencies: two dimensional np.array of type np.int32 or np.bool with one indicator vector per row
@@ -273,7 +273,7 @@ class SparseDSFTFunction(SetFunction):
     
 class DSFT3OneHop(SetFunction):
     
-    def __init__(self, n : int, weights, set_function : Callable[[npt.NDArray],float],model : str):
+    def __init__(self, n : int, weights, set_function,model):
         """
         @param n: integer for groundset size
         @param weights: ???
@@ -282,7 +282,7 @@ class DSFT3OneHop(SetFunction):
         """
         pass
     
-    def __call__(self, indicators : npt.NDArray, count_flag=True ):
+    def __call__(self, indicators, count_flag=True ):
         """
         @param indicators: two dimensional np.array of type np.int32 or np.bool
         with one indicator vector per row
@@ -297,72 +297,21 @@ class DSFT3OneHop(SetFunction):
 class DSFT4OneHop(SetFunction):
     
     def __init__(self, n, weights, set_function,model):
-        self.n = n
-        self.weights = weights
-        self.s = set_function
-        self.call_counter = 0
-        self.model = model
+        pass
     
     def __call__(self, indicators, count_flag=True, sample_optimal=True):
-        if len(indicators.shape) < 2:
-            indicators = indicators[np.newaxis, :]
-        
-        s = self.s
-        weights = self.weights
-        if sample_optimal:
-            res = []
-            for ind in indicators:
-                nc = ind.shape[0]-np.sum(ind)
-                if count_flag:
-                    self.call_counter += (nc + 1)
-                mask = ind.astype(np.int32)==0
-                ind_shifted = np.tile(ind, [nc, 1])
-                ind_shifted[:, mask] = np.eye(nc, dtype=ind.dtype)
-                ind_one_hop = np.concatenate((ind[np.newaxis], ind_shifted), axis=0)
-                weight_s0 = np.ones(1)*(1 + weights[True^mask].sum())
-                active_weights = np.concatenate([weight_s0, weights[mask]])
-                res += [(s(ind_one_hop)*active_weights).sum()]
-            res = np.asarray(res)
-        else:
-            res = s(indicators)
-            for i, weight in enumerate(weights):
-                ind_shifted = indicators.copy()
-                ind_shifted[:, i] = 1
-                res += weight*s(ind_shifted)
-            if count_flag:
-                self.call_counter += (self.n + 1) * indicators.shape[0]
-        return res
+        pass
+
     
     def convertCoefs(self, estimate):
-        freqs = estimate.freqs
-        coefs = estimate.coefs
-        coefs_new = []
-        freqs = freqs.astype(bool)
-        for key, value in zip(freqs, coefs):
-            coefs_new += [value/(1 + self.weights[True^key].sum())]
-        return SparseDSFTFunction(freqs.astype(np.int32), np.asarray(coefs_new),model = self.model)
+        pass
 
 class WHTOneHop(SetFunction):
     def __init__(self, n, weights, set_function,model ):
-        self.n = n
-        self.weights = weights
-        self.s = set_function
-        self.call_counter = 0
-        self.model = model
+        pass
     
     def __call__(self, indicators, count_flag=True):
-        if len(indicators.shape) < 2:
-            indicators = indicators[np.newaxis, :]
-        if count_flag:
-            self.call_counter += (self.n + 1) * indicators.shape[0]
-        s = self.s
-        weights = self.weights
-        res = s(indicators)
-        for i, weight in enumerate(weights):
-            ind_shifted = indicators.copy()
-            ind_shifted[:, i] = True^ind_shifted[:, i]
-            res += weight*s(ind_shifted)
-        return res
+        pass
     
 def eval_sf(gt : SetFunction, estimate : SetFunction, n : int, n_samples=1000, err_types=["rel"], custom_samples=None, p=0.5):
     """
@@ -372,40 +321,10 @@ def eval_sf(gt : SetFunction, estimate : SetFunction, n : int, n_samples=1000, e
         @param n_samples: number of random measurements for the evaluation
         @param err_type: either mae or relative reconstruction error
     """
-    if custom_samples is None:
-        ind = np.random.binomial(1, p, (n_samples, n)).astype(np.bool)
-    else:
-        ind = custom_samples
-    gt_vec = gt(ind, count_flag=False)
-    est_vec = estimate(ind, count_flag=False)
-    # print(f'ground truth: {gt_vec[:10]}')
-    # print(f'estimate: {est_vec[:10]}')
-    errors = []
-    for err_type in err_types:
-        if err_type=="mae":
-            errors += [(np.linalg.norm(gt_vec - est_vec, 1)/n_samples)]
-        elif err_type=="rel":
-            errors += [(np.linalg.norm(gt_vec - est_vec)/np.linalg.norm(gt_vec))]
-        elif err_type=="inf":
-            errors += [np.linalg.norm(gt_vec - est_vec, ord=np.inf)]
-        elif err_type=="res_quantiles":
-            errors += [np.quantile(np.abs(gt_vec - est_vec), [0.25, 0.5, 0.75])]
-        elif err_type=="quantiles":
-            errors += [np.quantile(np.abs(gt_vec), [0.25, 0.5, 0.75])]
-        elif err_type=="res":
-            errors += [gt_vec - est_vec]
-        elif err_type=="raw":
-            errors += [gt_vec, est_vec]
-        elif err_type=="R2":
-            gt_mean = np.mean(gt_vec)
-            errors += [1 - np.mean((est_vec - gt_vec)**2)/np.mean((gt_vec - gt_mean)**2)]
-        else:
-            raise NotImplementedError("Supported error types: mae, rel, inf, res_quantiles, quantiles")
-    return errors
-
+    pass
 def createRandomSparse(n, k, constructor, 
-                      rand_sets=lambda size: np.random.binomial(1, 0.2, size),
-                      rand_vals=lambda k: (-0.5+np.random.rand(k))*20):
+                      rand_sets,
+                      rand_vals):
     """
     @param n: size of the ground set
     @param k: desired sparsity
@@ -414,25 +333,7 @@ def createRandomSparse(n, k, constructor,
     @param rand_vals: a random Fourier coefficient generator
     @returns: a fourier sparse set function, the actual sparsity
     """
-    freq_coef_dict = dict()
-    freqs = rand_sets((k, n))
-    coefs = rand_vals(k)
-    coefs[coefs == 0] = 1
-    for freq, val in zip(freqs, coefs):
-        freq_coef_dict[tuple(freq.tolist())] = val
-    freq_coef_dict.pop(tuple(np.zeros(n, dtype=np.int32).tolist()), None)
-    freqs = np.asarray(list(freq_coef_dict.keys())).astype(np.int32)
-    coefs = np.asarray(list(freq_coef_dict.values())).astype(np.float64)
-    k = len(freqs)
-    return constructor(freqs, coefs)
+    pass
 
 def build_from_csv(path,model = None):
-    df = pd.read_csv(path) 
-    df_freqs = df.iloc[:,1:-1]
-    df_coefs = df.iloc[:,-1:]
-    freqs = df_freqs.to_numpy().astype(np.int32)
-    coefs = df_coefs.to_numpy().flatten().tolist()
-    if model != None:
-        return SparseDSFTFunction(freqs,coefs,model = model)
-    else:
-        return WrapSignal(coefs)
+    pass
